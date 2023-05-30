b1ab033b0a0dd7acce81689bafcadb611e72efc1
BooRuleDie <booruledie+github@gmail.com>
2023-05-30 08:36:41 +0300

    initial state

---
+++ sour/sour_acm412/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SourAcm412Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sour_acm412'
---
+++ sour/sour_acm412/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+import uuid
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class Topics(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50, unique=True)
+    origin_comment = models.CharField(max_length=1000)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comments(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    comment = models.CharField(max_length=1000)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    topic_id = models.ForeignKey(Topics, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+class Upvotes(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    # related_name="upvotes" thanks to this reverse relationship we are able to fetch all upvotes for each comment by using this syntax
+    # comment.upvotes.all()
+    comment_id = models.ForeignKey(Comments, on_delete=models.CASCADE, related_name='upvotes')
+
+    class Meta:
+        unique_together = ('user_id', 'comment_id')
+
+    def __str__(self):
+        return f'{self.user_id.username} upvoted {self.comment_id}'
+
+class Downvotes(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    comment_id = models.ForeignKey(Comments, on_delete=models.CASCADE, related_name='downvotes')
+
+    class Meta:
+        unique_together = ('user_id', 'comment_id')
+
+    def __str__(self):
+        return f'{self.user_id.username} downvoted {self.comment_id}'
